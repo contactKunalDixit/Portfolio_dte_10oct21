9a7ce76473473ea06c53a234f184bcce9165bf99
Kunal Dixit <contactkunaldixit@gmail.com>
2021-10-10 22:57:25 +0800

    Updated

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -4,8 +4,21 @@ import './Intro.scss';
 const Intro = () => {
 	return (
 		<div className='Intro'>
-			<div className='Left'></div>
-			<div className='Right'></div>
+			<div className='intro-left'>
+				<div className='intro-Left-Wrapper'>
+					<h2 className='i-intro'>Hello, My name is</h2>
+					<h1 className='i-name'>Kunal Dixit</h1>
+					<div className='i-title'>
+						<div className='i-title-Wrapper'>
+							<div className='i-title-Item'>Web Developer</div>
+							<div className='i-title-Item'>UI/UX Designer</div>
+							<div className='i-title-Item'>Content Creator</div>
+							<div className='i-title-Item'>Facilitator</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='intro-right'>Right</div>
 		</div>
 	);
 };
---
+++ src/components/Intro/Intro_StyledComp.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Intro = styled.div``;
+const Intro = styled.div``;
+const Intro = styled.div``;
+const Intro = styled.div``;
+
+const Intro_StyledComp = () => {
+	return (
+		<Intro>
+			<intro-Left></intro-Left>
+			<intro-Right></intro-Right>
+		</Intro>
+	);
+};
+
+export default Intro_StyledComp;

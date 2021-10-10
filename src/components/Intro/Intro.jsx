dab638952723282746a5087e2f75f3daccb9b2e6
Kunal Dixit <contactkunaldixit@gmail.com>
2021-10-10 18:44:45 +0800

    Initiated

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Intro.scss';
+
+const Intro = () => {
+	return (
+		<div className='Intro'>
+			<div className='Left'></div>
+			<div className='Right'></div>
+		</div>
+	);
+};
+
+export default Intro;

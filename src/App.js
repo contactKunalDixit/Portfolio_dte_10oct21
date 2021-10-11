f93ad056f267b3d61bff13b855f1a3e8482c2089
Kunal Dixit <contactkunaldixit@gmail.com>
2021-10-11 19:23:44 +0800

    Updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Intro from './components/Intro/Intro';
+import About from './components/About/About';
 
 function App() {
 	return (
 		<div className='App'>
 			<Intro></Intro>
+			<About></About>
 		</div>
 	);
 }

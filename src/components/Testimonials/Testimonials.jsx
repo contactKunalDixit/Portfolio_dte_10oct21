2dc8fd915c0d87fb9c90da08dc9ae079e0ac13d1
Kunal Dixit <contactkunaldixit@gmail.com>
2021-10-22 03:15:16 +0800

    Updated

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './testimonials.scss';
+
+const Testimonials = () => {
+	return (
+		<div className='testimonials'>
+			<div className='testimonials-Wrapper'>Hello World</div>
+		</div>
+	);
+};
+
+export default Testimonials;
